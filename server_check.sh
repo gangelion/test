9d2c0da634594a59bb3cebe7c668761efda2fb16
kojimatetusya <gangelion8@gmail.com>
2021-05-02 09:20:40 +0900

    a

--- server_check.sh
+++ server_check.sh
@@ -1,1 +1,16 @@
-echo "hello"
+#!/bin/bash
+
+url="https://my.uopeople.edu/"
+
+curl -I $url > check # -Iでヘッダーのみ出力しcheckに出力
+
+# grep HTTP check の出力 サーバーが正常な場合 HTTP/1.1 200 OK
+# awk '{print $2}'で2番目の200を取得
+STATUS_CODE=`grep HTTP check | awk '{print $2}'`
+
+if [ $STATUS_CODE -ne 200 ];then
+    curl -X POST -H 'Content-type: application/json' --data "{'text': 'Moodle落ちてます:cry:\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
+else
+    # テストのために一旦出力
+    curl -X POST -H 'Content-type: application/json' --data "{'text': '正常です。\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
+fi
