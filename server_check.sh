ea2af4f9ab6cd0c1f93b26ccf705218c9df2dbc8
kojimatetusya <gangelion8@gmail.com>
2021-05-02 09:55:32 +0900

    a

--- server_check.sh
+++ server_check.sh
@@ -7,9 +7,9 @@ curl -I $url > check # -Iでヘッダーのみ出力しcheckに出力
 # awk '{print $2}'で2番目の200を取得
 STATUS_CODE=`grep HTTP check | awk '{print $2}'`
 
-#if [ $STATUS_CODE -ne 200 ];then
-#    curl -X POST -H 'Content-type: application/json' --data "{'text': 'Moodle落ちてます:cry:\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
-#else
+if [ $STATUS_CODE -ne 200 ];then
+    curl -X POST -H 'Content-type: application/json' --data "{'text': 'Moodle落ちてます:cry:\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
+else
     # テストのために一旦出力
-#    curl -X POST -H 'Content-type: application/json' --data "{'text': '正常です。\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
-#fi
+    curl -X POST -H 'Content-type: application/json' --data "{'text': '正常です。\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
+fi
